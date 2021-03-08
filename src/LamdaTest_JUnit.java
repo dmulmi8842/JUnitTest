c51ea088284db977a680ec5b0bf768feaa4e2167
dmulmi8842 <addlelife@gmail.com>
2021-03-08 20:06:26 +0100

    Signup form test

--- src/LamdaTest_JUnit.java
+++ src/LamdaTest_JUnit.java
@@ -4,17 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LamdaTest_JUnit {
     WebDriver driver;
 
     @Test
-    public void Register_User()
-    {
+    public void Register_User() {
         System.setProperty("webdriver.chrome.driver", "C://Users//Diwash PC//Desktop//Selenium Learning//selenium jars and drivers//drivers//chromedriver.exe");
-        driver= new ChromeDriver() ;
+        driver = new ChromeDriver();
         driver.get("https://www.lambdatest.com");
         driver.manage().window().maximize();
         driver.findElement(By.xpath("//a[text()='Start Free Testing']")).click();
@@ -24,7 +25,7 @@ public class LamdaTest_JUnit {
         driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("9412262090");
         driver.findElement(By.xpath("//input[@type='checkbox']")).click();
         driver.findElement(By.xpath("//button[text()='Free Sign Up']")).click();
-        String url= driver.getCurrentUrl();
+        String url = driver.getCurrentUrl();
         assertEquals("https://accounts.lambdatest.com/email/verify", url);
         System.out.println(url);
         driver.close();
