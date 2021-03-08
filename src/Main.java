c6069b01efa29edad7fccdf224975976a80fd24b
dmulmi8842 <addlelife@gmail.com>
2021-03-08 01:31:45 +0100

    Remove unnecessary comments.

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ public class Main {
     @Test
     public static void main(String[] args) {
         WebDriver obj;
-        //System.out.println("hello world");
-        System.setProperty("webdriver.chrome.driver","C://Users//Diwash PC//Desktop//Selenium Learning//selenium jars and drivers//drivers//chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C://Users//Diwash PC//Desktop//Selenium Learning//selenium jars and drivers//drivers//chromedriver.exe");
         obj = new ChromeDriver();
         obj.get("https://www.google.com");
         String currentTitle = obj.getTitle();
